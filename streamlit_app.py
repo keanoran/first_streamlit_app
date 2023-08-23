e7a4a700807932474911a69b59b87ec50162c5aa
keanoran <142963055+keanoran@users.noreply.github.com>
2023-08-23 12:49:36 +0800

    Update streamlit_app.py
    added fourth line commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Brakfast Menu')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
